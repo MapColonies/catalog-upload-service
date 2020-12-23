6041b232e0e2e4153b2c6972d6ec5ce552b0350e
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2020-12-23 17:16:10 +0200

    feat(models): added support for local models (#14)
    * feat(models): added support for local models
    
    * docs(readme): updated readme
    
    * chore(clreanup): cleanup
    
    * chore(deps): updated models deps

--- src/controllers/swagger.ts
+++ src/controllers/swagger.ts
@@ -1,22 +1,24 @@
 import swaggerUi from 'swagger-ui-express';
 import { get } from 'config';
 import { MCLogger } from '@map-colonies/mc-logger';
-import { Request, Response, RequestHandler } from 'express';
+import { Request, Response, RequestHandler, NextFunction } from 'express';
 import { load } from 'yamljs';
-import { injectable, delay, inject } from 'tsyringe';
+import { delay, inject, injectable } from 'tsyringe';
 import { config as initDotEnv } from 'dotenv';
+import { resolveRefs } from 'json-refs';
+import httpStatus from 'http-status-codes';
 
-interface SwaggerServer  { 
-  [key: string]: unknown,
+interface SwaggerServer {
+  [key: string]: unknown;
   url: string;
-};
+}
 
 @injectable()
 export class SwaggerController {
   public uiMiddleware: RequestHandler[];
-  public serveUi: RequestHandler;
 
-  private readonly swaggerDoc: swaggerUi.JsonObject;
+  private swaggerDoc?: swaggerUi.JsonObject;
+  private uiHandler?: RequestHandler;
 
   private readonly swaggerConfig: {
     jsonPath: string;
@@ -28,21 +30,70 @@ export class SwaggerController {
   ) {
     this.swaggerConfig = get('swagger');
     // load swagger object from file
-    this.swaggerDoc = load('./docs/openapi3.yaml') as swaggerUi.JsonObject;
-    this.setSwaggerHost();
-    this.serveUi = swaggerUi.setup(this.swaggerDoc);
     this.uiMiddleware = swaggerUi.serve;
+    this.init().catch((err) => {
+      throw err;
+    });
+  }
+
+  public serveUi(req: Request, res: Response, next: NextFunction): void {
+    if (this.uiHandler) {
+      this.uiHandler(req, res, next);
+    } else {
+      res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
+    }
   }
 
   public serveJson(req: Request, res: Response): void {
     res.json(this.swaggerDoc);
   }
 
+  private async init(): Promise<void> {
+    this.swaggerDoc = (await this.multiFileSwagger(
+      load('./docs/openapi3.yaml')
+    ).catch((err) => {
+      throw err;
+    })) as swaggerUi.JsonObject;
+    this.setSwaggerHost();
+    this.uiHandler = swaggerUi.setup(this.swaggerDoc, {
+      swaggerOptions: { basePath: './Schema' },
+    });
+  }
+
   private setSwaggerHost(): void {
     initDotEnv();
     const host: string = process.env.HOST ?? 'http://localhost';
     const port: string = process.env.SERVER_PORT ?? '80';
-    const servers = ((this.swaggerDoc.servers) as SwaggerServer[]);
+    const doc = this.swaggerDoc as swaggerUi.JsonObject;
+    const servers = doc.servers as SwaggerServer[];
     servers[0].url = `${host}:${port}`;
+  }
+
+  private async multiFileSwagger(
+    root: Record<string, unknown>
+  ): Promise<void | Record<string, unknown>> {
+    const options = {
+      filter: ['relative', 'remote'],
+      location: './docs/openapi3.yaml',
+      loaderOptions: {
+        processContent: function (
+          res: { text: string },
+          callback: unknown
+        ): void {
+          const cb = callback as (a: null, b: unknown) => void;
+          cb(null, JSON.parse(res.text));
+        },
+      },
+    };
+
+    const logger = this.logger;
+    return resolveRefs(root, options).then(
+      function (results) {
+        return results.resolved as Record<string, unknown>;
+      },
+      function (err: Error) {
+        logger.warn(`failed to load swagger reference: ${err.message}.`);
+      }
+    );
   }
 }
